3e46d9887987e3120a6ddbf4fced2f957fa0fb53
BayalagmaaTeacher <126443558+BayalagmaaTeacher@users.noreply.github.com>
2023-02-27 14:07:18 +0800

    first commit yeaaaay

---
+++ News.js
@@ -0,0 +1,30 @@
+let newsList = [
+  {
+    title: "default medee",
+    body: "default body bla bla bla",
+  },
+];
+
+function displayNews() {
+  let newNewsList = document.getElementById("news_list");
+  let newDiv = "";
+
+  for (let i = 0; i < newsList.length; i++) {
+    newDiv += "<p>" + (i + 1) + ". " + newsList[i].title + "</p>" + "</br>";
+    newDiv += "<p>" + newsList[i].body + "</p>" + "</br>";
+  }
+  console.log(newDiv);
+  newNewsList.innerHTML = newDiv;
+}
+
+function addNews() {
+  let newNewsTitle = document.getElementById("news_title").value;
+  let newNewsBody = document.getElementById("news_body").value;
+  let newNews = {
+    title: newNewsTitle,
+    body: newNewsBody,
+  };
+  newsList.push(newNews);
+  console.log(newsList);
+  displayNews();
+}
